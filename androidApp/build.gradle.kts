0c3392ae6b1d042fcca4a4147b4cb76cc541bc14
Ronie Athaydes <ronieathaydes@gmail.com>
2021-05-27 15:28:34 -0300

    Update deprecated references

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -6,16 +6,16 @@ plugins {
 dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
 }
 
 android {
-    compileSdkVersion(30)
+    compileSdk = 30
     defaultConfig {
         applicationId = "com.england.android"
-        minSdkVersion(21)
-        targetSdkVersion(30)
+        minSdk = 21
+        targetSdk = 30
         versionCode = 1
         versionName = "1.0"
     }
